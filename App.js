89fb5c0200e5bbbfe63bb0045382683d4dfcfd11
avinashdubey2211 <142596784+avinashdubey2211@users.noreply.github.com>
2024-06-20 17:28:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import React from 'react'
+import Resturent from"./component/basic/Resturent"
+//import UseState from "./component/Hooks/UseState"
+//import UseEffect from './component/Hooks/UseEffect';
+// import UseReducer  from "./component/Hooks/UseReducer";
+
+const App = () => {
+  return (
+<Resturent />
+//<UseState/>
+
+//<UseEffect/>
+// {/* <UseReducer/> */}
+  
+);
+    
+  
+}
+
+export default App
